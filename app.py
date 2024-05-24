011d3d02cdd6a83105a32bd39ca9437bfb604b53
stenli <s.nevrovsky@ya.ru>
2024-05-24 20:48:41 +0300

    add example, and part of auth

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -8,10 +8,17 @@ def index():  # put application's code here
     return render_template("index.html")
 
 
-@app.route('/auth')
-def auth():  # put application's code here
+@app.route('/auth', method=["GET"])
+def send_auth():  # put application's code here
     return render_template("auth.html")
+
+@app.route('/auth', method=["POST"])
+def auth():  # put application's code here
+    code = request.form.get("authCode")
+    if not code:
+        return redirect("/auth")
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
