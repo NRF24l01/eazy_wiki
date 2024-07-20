ae41a052aca7b2030f8431cbad9281013a279903
nrf24l01 <s.nevrovsky@ya.ru>
2024-07-20 17:49:12 +0300

    Add docs saving in db and save md and logo via files

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import create_model
+from os import makedirs, path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -14,12 +17,8 @@ User, Wiki = create_model(db)
 migrate = Migrate(app, db, compare_type=True)
 
 app.secret_key = 'negrQWERTY123'
-
-
-def get_user_type_by_code(code):
-    print(User.query.filter_by(key=code).first())
-    return None  # Return None if the code is not found
-
+makedirs("instance/files/images", exist_ok=True)
+makedirs("instance/files/wikis", exist_ok=True)
 
 def article_edit():
     json_content = ""
@@ -32,10 +31,22 @@ def article_edit():
         json_content = {}
 
 
-def check_user(ses):
+def check_user_by_code(code):
+    if not User.query.filter_by(key=code).first(): return None
+    return User.query.filter_by(key=code).first()
+
+
+def check_user(ses) -> None:
     code = ses.get("code", None)
-    if not code: return None
-    return get_user_type_by_code(code)
+    if not User.query.filter_by(key=code).first(): return None
+    return User.query.filter_by(key=code).first()
+
+
+def can_write(ses):
+    if not check_user(ses): return None
+    if check_user(ses).can_write:
+        return True
+    return False
 
 
 def remove_leading_hashtags(text):
@@ -85,7 +96,7 @@ def auth():  # put application's code here
     code = request.form.get("authCode")
     if not code:
         return redirect("/auth")
-    user_type = get_user_type_by_code(code)
+    user_type = check_user_by_code(code)
     if not user_type:
         return render_template("auth.html", error="Код не найден")
     session["code"] = code
@@ -94,37 +105,51 @@ def auth():  # put application's code here
 
 @app.route("/new_page", methods=["GET"])
 def create_page():
-    if not session.get("code", None):
-        return redirect("/auth")
+    if not can_write(session):
+        abort(403)
     else:
         return render_template("edit_wiki.html", auth=True)
 
 
-@app.route("/subm_page", methods=["POST"])
-def subm_page():
-    if not session.get("code", None):
-        return redirect("/auth")
-    else:
-        return render_template("submit_wiki.html", auth=True)
-
-
 @app.route("/api/v1/send_md", methods=["POST"])
 def recive_md():
-    if not get_user_type_by_code(session.get("code", None)):
-        return jsonify({"state": False, "err": "please auth"})
+    if not can_write(session):
+        abort(403)
     else:
         markdown = request.get_json()["md"]
         print(markdown)
+        session["md"] = markdown
         if markdown:
-
-            return jsonify({"state": True, "link": "/"})
+            return jsonify({"state": True, "link": "/final_config_docs"})
         else:
             return jsonify({"state": False, "err": "Please enter markdown"})
 
 
-@app.route("/mu")
-def mu():
-    return render_template("modal_test.html")
+@app.route("/final_config_docs", methods=["GET"])
+def final_config_docs():
+    if not can_write(session):
+        abort(403)
+    else:
+        return render_template("final_config.html", auth=True, md=session.get("md", None))
+
+
+@app.route("/final_config_docs", methods=["POST"])
+def save_docs():
+    if not can_write(session):
+        abort(403)
+    else:
+        uploaded_file = request.files['cover']
+        filename = secure_filename(uploaded_file.filename)
+        wiki_title, desc = request.form.get("title"), request.form.get("desc")
+
+        with open(path.join("instance/files/wikis", wiki_title+".md"), "w", encoding="UTF-8") as f: f.write(session.get("md", ""))
+        uploaded_file.save(path.join("instance/files/images", filename))
+
+        new_wiki = Wiki(title=wiki_title, desc=desc, path_to_md=path.join("instance/files/wikis", wiki_title+".md"), path_to_logo=path.join("instance/files/images", filename))
+        db.session.add(new_wiki)
+        db.session.commit()
+
+        return redirect("/")
 
 
 if __name__ == '__main__':
--- wiki_md/ctd.md
+++
@@ -1,6 +0,0 @@
-# CTD
-## Запуск
-```bash
-sudo chmod 777 /dev/v4l/by-path/pci-0000\:00\:14.0-usb-0\:3\:1.0-video-index0
-sudo systemctl restart ctd.service
-```
